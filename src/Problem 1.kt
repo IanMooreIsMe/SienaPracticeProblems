3b1a58208a207f3c09b20237a9bf82ac691ed888
IanM-56 <privet.privet250@gmail.com>
2018-03-13 14:27:41 -0400

    Initial commit

---
+++ src/Problem 1.kt
@@ -0,0 +1,17 @@
+import kotlin.test.assertEquals
+
+object Problem1 {
+    fun run(value: Int): String {
+        return "$value ${icv(value, 0)}"
+    }
+    
+    private fun icv(value: Int, depth: Int): Int {
+        if (value == 1) return depth
+        return icv(if (value.rem(2) == 0) value / 2 else value * 3 + 1, depth + 1)
+    }
+}
+
+fun main(args: Array<String>) {
+    assertEquals("2017 68", Problem1.run(2017))
+    assertEquals("7 16", Problem1.run(7))
+}
---
+++ src/Problem 2.kt
@@ -0,0 +1,13 @@
+import kotlin.test.assertEquals
+
+object Problem2 {
+    fun run(m: Int, n: Int): Int {
+        return (1 .. m).map { it * (n - (m - it)) }.sum()
+    }
+}
+
+fun main(args: Array<String>) {
+    assertEquals(204, Problem2.run(8, 8))
+    assertEquals(20, Problem2.run(3, 4))
+    assertEquals(299, Problem2.run(2, 100))
+}
